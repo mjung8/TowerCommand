0dd0ba4f2b24933b6d2c3018ac32bd584267789d
mjung8 <mmkjung@gmail.com>
2019-08-19 21:30:21 -0400

    Add enemy spawning and damage

---
+++ Assets/Scripts/DestroyOnContact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    private GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = GameObject.FindObjectOfType<GameController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            gameController.damageTaken += 0.5f;
+            Debug.LogWarning("damaged");
+        }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,37 +5,61 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     [SerializeField]
-    private HealthBar healthBar;
-    [SerializeField]
-    private float healthBarSize = 1f;
+    private GameObject[] enemies;
+    public HealthBar healthBar;
+    private float healthBarSize;
     private float initialDuration = 30f;
+    private float maxWidth;
+    private float screenHeight = 10f;
+    public float damageTaken;
 
     // Start is called before the first frame update
     void Start()
     {
+        healthBarSize = 1f;
         healthBar.SetSize(healthBarSize);
+
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = Camera.main.ScreenToWorldPoint(upperCorner);
+        maxWidth = targetWidth.x;
+        StartCoroutine(Enemy());
+
     }
 
     private void Update()
     {
-        //if (healthBarSize > 0)
-        //{
-        //    //healthBarSize = Mathf.Lerp(healthBarSize, healthBarSize - 1 / 30f, Time.deltaTime * 5f);
-        //    healthBarSize -= Time.deltaTime * (1 / initialDuration);
-        //}
+        Debug.Log("damage taken: " + damageTaken);
+        if (damageTaken > 0 && healthBarSize > 0)
+        {
+            healthBarSize -= damageTaken / 7.5f;
+            if (healthBarSize < 0)
+                healthBarSize = 0;
 
-        //if (healthBarSize < 0)
-        //{
-        //    healthBarSize = 0;
-        //    // game over
-        //}
+            Debug.LogWarning("health: " + healthBarSize);
 
+            healthBar.SetSize(healthBarSize);
+            damageTaken = 0;
+        }
+
+        if (healthBarSize <= 0)
+        {
+            StopCoroutine(Enemy());
+        }
 
     }
 
-    private void FixedUpdate()
+    IEnumerator Enemy()
     {
-        //healthBar.SetSize(healthBarSize);
+        while (healthBarSize > 0)
+        {
+            GameObject enemy = enemies[0];
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth * 0.95f, maxWidth * 0.95f), screenHeight);
+
+            Instantiate(enemy, spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(0.25f, 1.25f));
+        }
     }
 
 }
