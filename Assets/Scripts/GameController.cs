83ee820d94bef63f85726a376bedd8a3b41ddc98
mjung8 <mmkjung@gmail.com>
2019-08-19 20:07:54 -0400

    Add background, tower, and health bar

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private HealthBar healthBar;
+    [SerializeField]
+    private float healthBarSize = 1f;
+    private float initialDuration = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.SetSize(healthBarSize);
+    }
+
+    private void Update()
+    {
+        //if (healthBarSize > 0)
+        //{
+        //    //healthBarSize = Mathf.Lerp(healthBarSize, healthBarSize - 1 / 30f, Time.deltaTime * 5f);
+        //    healthBarSize -= Time.deltaTime * (1 / initialDuration);
+        //}
+
+        //if (healthBarSize < 0)
+        //{
+        //    healthBarSize = 0;
+        //    // game over
+        //}
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        //healthBar.SetSize(healthBarSize);
+    }
+
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    float xScaleMultiplier = 7.5f;
+    private Transform bar;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(sizeNormalized * xScaleMultiplier, 1f);
+    }
+
+}
